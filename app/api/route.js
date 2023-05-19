512bd39b7b704f504577d5e42dfbe82860e2bfaf
shaneski <shane@micronetclub.co.jp>
2023-05-19 14:43:39 +0900

    add token count check

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,11 @@
 import { chatCompletion } from "../../services/openai"
+import { isEven } from "../../lib/utils"
+
+/**
+ * API has 4096 max tokens but we are going to set out limit
+ * to much lower. You can change this if you want.
+ */
+const MAX_TOKENS = 3072 // 1024 x 3
 
 export async function POST(request) {
     
@@ -22,7 +29,31 @@ export async function POST(request) {
         })
     }
 
+    let prev_data = previous
     let text = ''
+
+    /**
+     * check token count
+     */
+    let total_token_count = Math.ceil((inquiry.length + system.length) / 4)
+    if(previous.length > 0) {
+        total_token_count += Math.ceil((JSON.stringify(previous)).length / 4)
+    }
+
+    if(total_token_count > MAX_TOKENS) {
+
+        /**
+         * We will delete old entries from previous data.
+         * This is just a very simple and crude way to ensure that we do not hit the maximum.
+         */
+        //let cutoff = parseInt(previous.length / 2)
+        let cutoff = previous.length > 40 ? Math.ceil(previous.length - 20) : parseInt(previous.length / 2)
+
+        cutoff = isEven(cutoff) ? cutoff : cutoff + 1
+
+        prev_data = previous.slice(cutoff)
+
+    }
 
     try {
         
@@ -35,7 +66,8 @@ export async function POST(request) {
             { role: 'system', content: system_prompt },
         ]
 
-        messages = messages.concat(previous)
+        //messages = messages.concat(previous)
+        messages = messages.concat(prev_data)
         messages.push({ role: 'user', content: inquiry })
 
         text = await chatCompletion({
